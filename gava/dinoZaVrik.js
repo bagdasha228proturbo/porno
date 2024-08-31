f48a2ce5b7e476350ab068728a0a087973458fa1
bogdan <bogdanivanovskiy4549@gmail.com>
2024-08-31 12:02:37 +0300

    діно

--- gava/dinoZaVrik.js
+++ gava/dinoZaVrik.js
@@ -1,9 +1,13 @@
 const dino = document.getElementById("dino")
 const cactus = document.getElementById("cactus")
 
-document.addEventListener("keydown", function(event) {
-    jump()
+document.addEventListener('keydown', function(start){
+    if (start.key === 'Enter') {
+        document.getElementById("dino").style.animationPlayState = 'running';
+        document.getElementById("cactus").style.animationPlayState = 'running';
+    }
 })
+
 
 function jump() {
     if (dino.classList != 'jump') {
@@ -12,14 +16,28 @@ function jump() {
     setTimeout( function() {
         dino.classList.remove('jump')
     }, 1000)
-
+    
 }
+
+document.addEventListener("keydown", function(event) {
+    jump()
+})
+
  let dinaGizn = setInterval(function(){
     let dinaTop = parseInt(window.getComputedStyle(dino).getPropertyValue('top'))
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue('left'))
 
     if (cactusLeft < 40 && cactusLeft > 0 && dinaTop >= 140) {
-        
+        document.getElementById("cactus").style.animation = 'none';
+        clearInterval(dinaGizn);
     }
- })
+    // if (document.addEventListener('keydown', function(){
+    //     if (start.key === 'Enter') {
+    //         document.getElementById("dino").style.animationPlayState = 'running';
+    //         document.getElementById("cactus").style.animationPlayState = 'running';
+    //         return
+    //     }
+
+    // }));
+ }, 10)
 
